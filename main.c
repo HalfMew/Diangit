10d309b08978fb8f54f7b56f1b1e6e24db0ab213
HalfMew <hq2897390078@qq.com>
2024-09-18 22:21:07 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,37 +3,8 @@
 #include <string.h>
 #include <sys/stat.h>
 
-void createGitDirectory(const char *directory) {
-    char gitDirPath[256];
-    snprintf(gitDirPath, sizeof(gitDirPath), "%s/.git", directory);
 
-    struct stat st = {0};
-    if (stat(gitDirPath, &st) == 0) {
-        printf("Error: Directory '%s' is already a Git repository.\n", directory);
-        return;
-    }
-
-    if (mkdir(gitDirPath, 0755) == -1) {
-        perror("Error creating.git directory");
-        return;
-    }
-
-    char configFilePath[256];
-    snprintf(configFilePath, sizeof(configFilePath), "%s/.git/config", directory);
-    FILE *configFile = fopen(configFilePath, "w");
-    if (configFile == NULL) {
-        perror("Error creating config file");
-        return;
-    }
-    fprintf(configFile, "[core]\nrepositoryformatversion = 0\nfilemode = true\n");
-    fclose(configFile);
-}
-
-int main(int argc, char *argv[]) {
-    const char *directory = ".";
-    if (argc > 1) {
-        directory = argv[1];
-    }
-    createGitDirectory(directory);
+int main() {
+printf("%s","Helloworld");
     return 0;
 }
